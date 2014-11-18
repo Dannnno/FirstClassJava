ccbaba70fbc2a6387d1904abcbe483b013541856
Dannnno <dannnno16@gmail.com>
2014-11-17 19:32:55 -0800

    Made adder work
    Figured out why I couldn't get adder to work

--- Functors/src/Adder.java
+++ Functors/src/Adder.java
@@ -1,10 +1,21 @@
 public class Adder extends HigherOrderFunctions.Functor {
-	
-		public Object run(Object...objects) {
+
+	public Object run(Object... objects) {
 		return (Integer) objects[0] + new Integer(1);
 	}
 	
-	public static void main() {
-		System.out.println(HigherOrderFunctions.map(new Adder(), new Integer[4]));
+	public static void printArray(Object[] array) {
+		String a = "{ ";
+		for(Object o: array) {
+			a += o.toString() + ", ";
+		}
+		a = a.substring(0, a.length()-2);
+		a += " }";
+		System.out.println(a);
+	}
+
+	public static void main(String[] args) {
+		Integer[] i = { 1, 2, 3, 4 };
+		printArray(HigherOrderFunctions.map(new Adder(), i));
 	}
 }
