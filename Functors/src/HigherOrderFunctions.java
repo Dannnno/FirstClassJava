a84b0dee9d1477d8197f2521c11dab48eb5c446e
Dannnno <dannnno16@gmail.com>
2014-11-24 22:24:01 -0800

    Improved iterable support
    Decided instead of wasting time trying to get reflection working I'd
    just make it return a List<Object> by default.  I might make an iterator
    one later? (a la itertools).  Going to look at how to do this with
    mapping types

--- Functors/src/HigherOrderFunctions.java
+++ Functors/src/HigherOrderFunctions.java
@@ -1,60 +1,29 @@
-// TODO: Figure out how to do something like "asList" instead of "toArray"
-// way to convert primitive types to their class equivalents
-// ideally in a single method, not likely though...
-// primitive types: byte, short, int, long, float, double, boolean, char
-
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-
-//import java.lang.Class;
-//import java.lang.reflect.Method;
-//import java.lang.reflect.Field;
 
 public class HigherOrderFunctions {
 
 	interface Callable {
 		abstract Object run(Object... objs);
 	}
-	
+
 	abstract static class Functor {
 		abstract Object run(Object... objs);
 	}
 	
+	abstract static class FunctorAble {
+		abstract Functor returnFunction();
+	}
+
 	private static Object[] iterableAsArray(Iterable<Object> obj) {
 		ArrayList<Object> tempArrayList = new ArrayList<Object>();
-		for(Object next: obj) {
+		for (Object next : obj) {
 			tempArrayList.add(next);
 		}
 		return tempArrayList.toArray();
 	}
-	
-//	private static Class getIterableClass(Iterable<Object> obj) {
-//		Class classOf = obj.getClass();
-//		System.out.println(classOf);
-//		return classOf;
-//	}
-//	
-//	private static Iterable<Object> castIterable(Iterable<Object> obj, Class clazz) {
-//		if(obj.getClass().equals(clazz)) {
-//			return obj;
-//		} else {
-//			try {
-//				Object iter = clazz.newInstance();
-//				(Iterable<Object>) iter;
-//	        } catch (ClassNotFoundException e) {
-//	            e.printStackTrace();
-//	        } catch (NoSuchMethodException nsme){
-//	            nsme.printStackTrace();
-//	        } catch (IllegalAccessException iae) {
-//	            iae.printStackTrace();
-//	        } catch (InstantiationException ie) {
-//	            ie.printStackTrace();
-//	        } 
-//	    }
-//			return new ArrayList<Object>();
-//		}
-//	}
 
 	public static Object[] map(Functor function, Object[] obj) {
 		Object[] newList = new Object[obj.length];
@@ -63,14 +32,17 @@ public class HigherOrderFunctions {
 		}
 		return newList;
 	}
-	
-	public static Iterable<Object> map(Functor function, Iterable<Object> obj) {
-		Object[] asArray = iterableAsArray(obj);
-		return Arrays.asList(map(function, asArray));
+
+	public static List<Object> map(Functor function, Iterable<Object> obj) {
+		List<Object> objList = new ArrayList<Object>();
+		for (Object o : obj) {
+			objList.add(o);
+		}
+		return objList;
 	}
 
 	public static Object[] filter(Functor function, Object[] obj) {
-		ArrayList<Object> filteredList = new ArrayList<Object>();
+		List<Object> filteredList = new ArrayList<Object>();
 		for (int i = 0; i < obj.length; i++) {
 			if ((Boolean) function.run(obj[i])) {
 				filteredList.add(obj[i]);
@@ -78,10 +50,15 @@ public class HigherOrderFunctions {
 		}
 		return filteredList.toArray();
 	}
-	
-	public static Iterable<Object> filter(Functor function, Iterable<Object> obj) {
-		Object[] asArray = iterableAsArray(obj);
-		return Arrays.asList(filter(function, asArray));
+
+	public static List<Object> filter(Functor function, Iterable<Object> obj) {
+		List<Object> filteredList = new ArrayList<Object>();
+		for (Object o : obj) {
+			if ((Boolean) function.run(o)) {
+				filteredList.add(o);
+			}
+		}
+		return filteredList;
 	}
 
 	public static Object foldr(Functor function, Object startingPoint,
@@ -92,10 +69,15 @@ public class HigherOrderFunctions {
 		}
 		return result;
 	}
-	
-	public static Object foldr(Functor function, Object startingPoint, Iterable<Object> obj) {
-		Object[] asArray = iterableAsArray(obj);
-		return foldr(function, startingPoint, asArray);
+
+	public static Object foldr(Functor function, Object startingPoint,
+			Iterable<Object> obj) {
+		Object result = startingPoint;
+		Collections.reverse((List<Object>) obj);
+		for (Object o : obj) {
+			result = function.run(result, o);
+		}
+		return result;
 	}
 
 	public static Object foldl(Functor function, Object startingPoint,
@@ -106,30 +88,37 @@ public class HigherOrderFunctions {
 		}
 		return result;
 	}
-	
-	public static Object foldl(Functor function, Object startingPoint, Iterable<Object> obj) {
-		Object[] asArray = iterableAsArray(obj);
-		return foldl(function, startingPoint, asArray);
+
+	public static Object foldl(Functor function, Object startingPoint,
+			Iterable<Object> obj) {
+		Object result = startingPoint;
+		for (Object o : obj) {
+			result = function.run(result, o);
+		}
+		return result;
 	}
-	
+
 	public static Object foldr(Functor function, Object[] obj) {
-		Object startingPoint = obj[obj.length-1];
-		List<Object> asList = Arrays.asList(obj);
-		List<Object> subList = asList.subList(0, obj.length-2);
-		return foldr(function, startingPoint, subList.toArray());		
+		Object startingPoint = obj[obj.length - 1];
+		List<Object> subList = Arrays.asList(obj).subList(0, obj.length - 2);
+		return foldr(function, startingPoint, subList.toArray());
+	}
+
+	public static Object foldl(Functor function, Object[] obj) {
+		Object startingPoint = obj[0];
+		List<Object> subList = Arrays.asList(obj).subList(1, obj.length - 1);
+		return foldl(function, startingPoint, subList.toArray());
 	}
 	
+	/* 
+	 * These two methods are not recommended.  There isn't a good way to 
+	 * get the first object from an iterable (reliably) that I've found yet,
+	 * so it has to make an array out of it.
+	 */
 	public static Object foldr(Functor function, Iterable<Object> obj) {
 		return foldr(function, iterableAsArray(obj));
 	}
-	
-	public static Object foldl(Functor function, Object[] obj) {
-		Object startingPoint = obj[0];
-		List<Object> asList = Arrays.asList(obj);
-		List<Object> subList = asList.subList(1, obj.length-1);
-		return foldl(function, startingPoint, subList.toArray());		
-	}
-	
+
 	public static Object foldl(Functor function, Iterable<Object> obj) {
 		return foldl(function, iterableAsArray(obj));
 	}
